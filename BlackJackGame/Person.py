102001dcc905ef613aa5935a840fff882e5e60d5
shalini47ch <shalini47choudhary@gmail.com>
2024-09-10 16:55:21 +0530

    Person class added

---
+++ BlackJackGame/Person.py
@@ -0,0 +1,28 @@
+#here it will have parameters like name,city,country,zipcode,streetAddress,state
+
+class Person:
+    def __init__(self,name,city,country,zipcode,streetAddress,state):
+        self.name=name
+        self.city=city
+        self.country=country
+        self.zipcode=zipcode
+        self.streetAddress=streetAddress
+        self.state=state
+    
+    def getName(self):
+        return self.name
+    
+    def getCity(self):
+        return self.city
+    
+    def getCountry(self):
+        return self.country
+    
+    def getZipCode(self):
+        return self.zipcode
+    
+    def getstreetAddress(self):
+        return self.streetAddress
+    
+    def getState(self):
+        return self.state
